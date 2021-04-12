4b6c6d4ae95ddc75aa0d0b0c9cfae632f5b179e6
samankassou <samankassoufoulla@gmail.com>
2021-04-12 13:16:27 +0200

    Update AcademicYear model, set timestamps to false and define array of dates columns

--- app/Models/AcademicYear.php
+++ app/Models/AcademicYear.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class AcademicYear extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $dates = ['start_date', 'end_date'];
 }
