5f2ed7f563fe224e6c8c55d2f9a06d21e23199ae
samankassou <samankassoufoulla@gmail.com>
2021-04-21 00:05:31 +0200

    Update the design of the user details view

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -7,17 +7,15 @@
             <h4 class="card-title">Informations de {{ $user->name }}</h4>
         </div>
         <div class="card-body">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card-content d-flex">
-                        <img src="{{ asset(empty($user->avatar)? 'images/default-user.jpg' : $user->avatar->getUrl()) }}" class="card-img-top img-fluid" alt="singleminded">
-                        <div class="card-body">
-                            <h5 class="card-title">Noms: {{ $user->name }}</h5>
-                            <h5 class="card-title">Email: {{ $user->email }}</h5>
-                            <h5 class="card-title">Statut: {{ $user->status == 1?'Actif':'Inactif' }}</h5>
-                            
-                        </div>
-                    </div>
+            <div class="card-content d-flex">
+                <div class="w-25">
+                    <img src="{{ asset(empty($user->avatar)? 'images/default-user.jpg' : $user->avatar->getUrl()) }}" class="mw-100 rounded" alt="singleminded">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Noms: {{ $user->name }}</h5>
+                    <h5 class="card-title">Email: {{ $user->email }}</h5>
+                    <h5 class="card-title">Statut: {{ $user->status == 1?'Actif':'Inactif' }}</h5>
+                    
                 </div>
             </div>
         </div>
