072f19a8e2b6709c761c61a23a29f8134a4c443a
samankassou <samankassoufoulla@gmail.com>
2021-04-21 14:39:22 +0200

    Make datatables responsive

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,6 +2,7 @@
 @section('styles')
 <link rel="stylesheet" href="{{ asset('mazer/assets/vendors/toastify/toastify.css') }}">
 <link href="{{ asset('vendor/datatables/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/datatables/css/responsive.dataTables.min.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -113,6 +114,7 @@
 @section('scripts')
 <script src="{{ asset('vendor/datatables/js/jquery-3.5.1.js') }}"></script>
 <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/datatables/js/dataTables.responsive.min.js') }}"></script>
 <script src="{{ asset('vendor/datatables/js/dataTables.bootstrap5.min.js') }}"></script>
 <script src="{{ asset('mazer/assets/vendors/toastify/toastify.js') }}"></script>
 <script>
@@ -128,6 +130,7 @@
         language: {
             url: "{{ asset('vendor/datatables/lang/French.json') }}"
         },
+        responsive: true,
         processing: true,
         serverSide: true,
         ajax: {
