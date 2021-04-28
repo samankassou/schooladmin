4cc948d2c9dd7660b39c43b9842b7be61afeaab0
samankassou <samankassoufoulla@gmail.com>
2021-04-28 15:02:39 +0200

    Add dummy data in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,86 @@
 @extends('layouts.app', ['title' => 'Dashboard'])
 
+@section('styles')
+<link rel="stylesheet" href="{{ asset('mazer/assets/vendors/iconly/bold.css') }}">
+@endsection
+
 @section('content')
 <section class="section">
     <div class="card">
         <div class="card-header">
-            <h4 class="card-title">Example Content</h4>
+            <h4 class="card-title">Dashboard</h4>
         </div>
         <div class="card-body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quas omnis
-            laudantium tempore
-            exercitationem, expedita aspernatur sed officia asperiores unde tempora maxime odio
-            reprehenderit
-            distinctio incidunt! Vel aspernatur dicta consequatur!
+            <div class="row">
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon purple">
+                                        <i class="iconly-boldShow"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Elèves</h6>
+                                    <h6 class="font-extrabold mb-0">2500</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon green">
+                                        <i class="iconly-boldAdd-User"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Enseignants</h6>
+                                    <h6 class="font-extrabold mb-0">45</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon blue">
+                                        <i class="iconly-boldProfile"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Salles de classes</h6>
+                                    <h6 class="font-extrabold mb-0">32</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon red">
+                                        <i class="iconly-boldBookmark"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Surveillants</h6>
+                                    <h6 class="font-extrabold mb-0">8</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
