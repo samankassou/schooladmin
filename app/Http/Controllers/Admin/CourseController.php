7e644ebd9fc2ef6a7f67df93fce69aa623fdf43b
samankassou <samankassoufoulla@gmail.com>
2021-05-05 16:54:45 +0200

    Add the 'create course' feature

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -7,6 +7,7 @@ use App\Models\AcademicYear;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCourseRequest;
 
 class CourseController extends Controller
 {
@@ -32,24 +33,15 @@ class CourseController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreCourseRequest $request)
     {
-        //
+        Course::create($request->validated() + ['academic_year_id' => AcademicYear::current()->id]);
+        return response()->json(['message' => 'Course created successfully!']);
     }
 
     /**
@@ -59,17 +51,6 @@ class CourseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Course $course)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Course  $course
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Course $course)
     {
         //
     }
--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -26,6 +26,40 @@
         </div>
     </div>
 </section>
+{{-- Create course modal --}}
+<div class="modal fade text-left" id="create-course-modal" tabindex="-1" aria-labelledby="myModalLabel33" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel33">Ajouter une matière </h4>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <i data-feather="x"></i>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="#" id="create-course-form">
+                    <div class="form-group">
+                        <input type="text" id="name" placeholder="Nom de la matière" class="form-control" name="name">
+                        <div class="invalid-feedback" id="name-error">
+                            
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                    <i class="bx bx-x d-block d-sm-none"></i>
+                    <span class="d-none d-sm-block">Annuler</span>
+                </button>
+                <button id="save-course-btn" type="button" class="btn btn-primary ml-1">
+                    <i class="bx bx-check d-block d-sm-none"></i>
+                    <span class="d-none d-sm-block">Enregistrer</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+{{--! Create course modal --}}
 @endsection
 @section('scripts')
 <script src="{{ asset('vendor/datatables/js/jquery-3.5.1.js') }}"></script>
@@ -63,5 +97,40 @@
             },
         ]
     });
+
+    $('#save-course-btn').click(function(e){
+        var data = $('#create-course-form').serialize();
+        $.ajax({
+            method: "POST",
+            url: "{{ route('admin.courses.store') }}",
+            data: data,
+            success: function(response){
+                reset_modal();
+                $('#create-course-modal').modal('hide');
+                table.ajax.reload(null, false);
+                Toastify({
+                    text: "Matière enregistrée avec succès!",
+                    duration: 3000,
+                    close:true,
+                    gravity:"top",
+                    position: "right",
+                    backgroundColor: "#4fbe87",
+                }).showToast();
+            },
+            error: function(response){
+                var errors = response.responseJSON.errors;
+                for (const error in errors) {
+                    $('#'+error+'-error').html(errors[error][0]).show();
+                }
+            }
+        });
+    });
+
+    function reset_modal()
+    {
+        $('#create-course-form').trigger("reset");
+        $('#edit-course-form').trigger("reset");
+        $("[id$='-error']").html('');
+    }
 </script>
 @endsection
